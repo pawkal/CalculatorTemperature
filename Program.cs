7d26989dddae2433cf894c0d86d26c7cbcbef946
pawkal <kalel7410@gmail.com>
2023-01-26 19:49:25 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTemperature
+{
+    internal class CalcTemperature
+    {
+        static void Main(string[] args)
+        {
+            double F, C;
+            Console.WriteLine("Podaj temperature w stopniach Fahrenheita");
+            F = double.Parse(Console.ReadLine());
+            C = 5.0 / 9 * (F - 32);
+            Console.WriteLine(C + "  Temperatura w stopniach Celsjusza");
+            Console.ReadKey();
+        }
+    }
+}
